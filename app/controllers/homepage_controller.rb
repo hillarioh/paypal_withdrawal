78e271caca71918008b32e8ec791eeab7610858d
hillarioh <okerioh@gmail.com>
2021-07-27 16:52:12 +0300

    render xml

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,6 +1,24 @@
 class HomepageController < ApplicationController
   def index
   end
+  def credentials
+    data={
+      "app": {
+        "id": "254qwerty",
+        "name": "PayPal Withdrawal",
+        "provider": "Finplus group",
+        "description": "A tool to export your data in an xbrl report ready to use for mix market",
+        "uninstallUrl": "https://mixmarketapp.appspot.com/mambuxbrl/uninstall",
+        "installUrl": "httsp://mixmarketapp.appspot.com/mambuxbrl/install",
+        "extensionpoint": {
+          "location": "REPORTING_VIEW",
+          "label": "PAYPAL Withdrawal",
+          "url": "https://a06cf2f6381d.ngrok.io",
+        }
+      }
+    }
+     render :xml =>  data      
+  end
   def withdraw 
     @ksh_withdrawal = Withdraw.without_conversion(params[:amount])
     @usd_withdrawal = Withdraw.with_conversion(params[:amount])
